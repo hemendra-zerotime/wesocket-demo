85e03b45aa0b8f21413e87b7ef80951420f55810
Hemendra Zerotime <hemendra.zerotimesolutions@gmail.com>
2025-01-21 17:05:59 +0530

    add notification feature

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,34 @@
-import express from "express"
-import http from "http"
-import path from "path"
-import { Server } from "socket.io"
+import express from "express";
+import http from "http";
+import path from "path";
+import { Server } from "socket.io";
 
-const app = express()
-const port = 3000
-const server = http.createServer(app)
-const io = new Server(server)
+const app = express();
+const port = 3000;
+const server = http.createServer(app);
+const io = new Server(server);
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"))
-})
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 io.on("connection", (socket) => {
-    console.log("a user connected")
+    console.log("a user connected");
+
     socket.on("message", (message) => {
-        console.log(`${socket.id} says ${message}`)
-        socket.broadcast.emit("message", message)
-    })
-})
+        console.log(`${socket.id} says ${message}`);
+        socket.broadcast.emit("message", message);
+    });
+
+    const notifiCation = setInterval(() => {
+        let not = "your connection is live";
+        socket.broadcast.emit("notification", not);
+    }, 10000);
+
+    socket.on("disconnect", () => {
+        clearInterval(notifiCation);
+    });
+});
 
 server.listen(port, () => {
-    console.log("server started at port", port)
-})
-
+    console.log("server started at port", port);
+});
